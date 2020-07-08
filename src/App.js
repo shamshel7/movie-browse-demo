a44f5ca4163cc4a1b2f8a87ff6adbe7a29bf354f
Tomas Baer <tom.baer@yahoo.co.uk>
2020-07-08 11:49:14 +0200

    Changed sections to become carousels

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tile from './Tile';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const apiKey = "6cd24402a0302e3560524e21ec35bb5d";
 const familyGenreId = 10751;
 const documentaryGenreId = 99;
+
+//var noOfTiles = Math.floor(this.state.width / 250);
 
 const urls = {
     popular_movies: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
@@ -21,12 +25,23 @@ class App extends Component {
             popularMovies: [],
             popularSeries: [],
             family: [],
-            documentary: []
+            documentary: [],
+            width: 0,
+            noOfTiles: 5
         }
+
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
 
     componentDidMount() {
         this.getIds();
+
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
     }
 
     /**
@@ -34,35 +49,83 @@ class App extends Component {
      * @param {string} type 
      */
     renderTiles(type) {
+        var responsive = {
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: this.state.noOfTiles,
+              slidesToSlide: 5, // optional, default to 1.
+              partialVisibilityGutter: 40
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 2,
+              slidesToSlide: 2 // optional, default to 1.
+            },
+            mobile: {
+              breakpoint: { max: 464, min: 0 },
+              items: 1,
+              slidesToSlide: 1 // optional, default to 1.
+            }
+        };
+
+        var tiles;
         switch (type) {
             case "PopularMovies":
-                return (
+                tiles = (
                     this.state.popularMovies.map((id) => (
                         <Tile key={id} id={id} type={"movie"}/>
                     ))
                 );
+                break;
             case "PopularSeries":
-                return (
+                tiles = (
                     this.state.popularSeries.map((id) => (
                         <Tile key={id} id={id} type={"tv"}/>
                     ))
                 );
+                break;
             case "Family":
-                return (
+                tiles = (
                     this.state.family.map((id) => (
                         <Tile key={id} id={id} type={"movie"}/>
                     ))
                 );
+                break
             case "Documentary":
-                return (
+                tiles = (
                     this.state.documentary.map((id) => (
                         <Tile key={id} id={id} type={"movie"}/>
                     ))
                 );
+                break;
             default:
                 console.log("No IDs found!");
                 break;
         }
+        
+        return (
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                showDots={true}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                infinite={false}
+                autoPlay={false}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                /* transitionDuration={500} */
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                deviceType={this.props.deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+                centerMode={true}
+            >
+                {tiles}
+            </Carousel>
+        );
     }
     
     render() {
@@ -142,6 +205,12 @@ class App extends Component {
             });
         });
     }
+
+    updateWindowDimensions() {
+        this.setState({
+            noOfTiles: Math.floor(window.innerWidth / 280)
+        });
+      }
 }
 
 export default App;
